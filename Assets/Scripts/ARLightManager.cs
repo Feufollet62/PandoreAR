62dafd434f656eeb7b6579e5b61158e5ba790af5
Eliot <fromenteliot@gmail.com>
2022-07-30 19:08:18 +0200

    Début estimation light

---
+++ Assets/Scripts/ARLightManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARLightManager : MonoBehaviour
+{
+    private ARCameraManager arCam;
+
+    private void Start()
+    {
+        //arCam.
+    }
+}
