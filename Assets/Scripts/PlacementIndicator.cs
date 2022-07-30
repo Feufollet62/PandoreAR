5bd24b1b8cc50dac9b1bb92be31f13a17915fe85
Eliot <fromenteliot@gmail.com>
2022-07-30 17:48:38 +0200

    Animated placement indicator

---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    [SerializeField] private Transform arrow;
+
+    [SerializeField] private AnimationCurve curve;
+    
+    private void Update()
+    {
+        float yValue = curve.Evaluate(Time.time);
+        arrow.localPosition = new Vector3(0, yValue, 0);
+    }
+}
